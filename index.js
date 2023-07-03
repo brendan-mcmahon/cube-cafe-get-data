498d588c4155c779d7f1f1408797cad7b4ea9029
brendanjeffreymcmahon <brendan.mcmahon@bloomerang.com>
2023-07-02 23:15:24 -0400

    ad logging

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 
 exports.handler = async (event) => {
   try {
     const dynamodb = new AWS.DynamoDB.DocumentClient();
     const params = {
-      TableName: 'cube-cafe-data'
+      TableName: "cube-cafe-data",
     };
 
     const data = await dynamodb.scan(params).promise();
@@ -13,21 +13,21 @@ exports.handler = async (event) => {
       statusCode: 200,
       body: JSON.stringify(data.Items),
       headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'Content-Type',
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
-        'Content-Type': 'application/json'
-      }
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+        "Content-Type": "application/json",
+      },
     };
   } catch (error) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: 'An error occurred while fetching the data' }),
+      body: JSON.stringify({ error: `An error occurred while fetching the data: ${error}` }),
       headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'Content-Type',
-        'Content-Type': 'application/json'
-      }
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Content-Type": "application/json",
+      },
     };
   }
 };
