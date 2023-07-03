ab2ecadf4fab91b234c6b4f8bc8b578e2b8fe222
brendanjeffreymcmahon <brendan.mcmahon@bloomerang.com>
2023-07-02 23:09:32 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const AWS = require('aws-sdk');
+
+exports.handler = async (event) => {
+  try {
+    const dynamodb = new AWS.DynamoDB.DocumentClient();
+    const params = {
+      TableName: 'cube-cafe-data'
+    };
+
+    const data = await dynamodb.scan(params).promise();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data.Items),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+        'Content-Type': 'application/json'
+      }
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'An error occurred while fetching the data' }),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Content-Type': 'application/json'
+      }
+    };
+  }
+};
